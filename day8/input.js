6adf2a3b9355b61c22fd76ddaed5dd8e01471509
Igor Gregori <igsilvacarlos@gmail.com>
2023-12-10 22:13:57 -0300

    resolução dia 8 parte 1 e 2

---
+++ day8/input.js
@@ -0,0 +1,16 @@
+const instructions = ['R', 'L']
+
+const map = {
+    'AAA': ['BBB', 'CCC'],
+    'BBB': ['DDD', 'EEE'],
+    'CCC': ['ZZZ', 'GGG'],
+    'DDD': ['DDD', 'DDD'],
+    'EEE': ['EEE', 'EEE'],
+    'GGG': ['GGG', 'GGG'],
+    'ZZZ': ['ZZZ', 'ZZZ']
+}
+
+module.exports = {
+    instructions,
+    map
+}
---
+++ day8/part1.js
@@ -0,0 +1,18 @@
+const { instructions, map } = require('./input_final')
+
+let step = 1
+
+function getInstruction(step) {
+    const instruction = instructions[step % instructions.length]
+    return instruction === 'L' ? 0 : 1
+}
+
+let node = 'AAA'
+
+while (true) {
+    node = map[node][getInstruction(step - 1)]
+    if (node === 'ZZZ') break
+    step++
+}
+
+console.info('step:', step)
