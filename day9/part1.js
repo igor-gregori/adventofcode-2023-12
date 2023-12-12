3f4d997eb6b1de6707fc4b35a597098c5f282c8b
Igor Gregori <igsilvacarlos@gmail.com>
2023-12-11 23:35:09 -0300

    resolução dia 9 parte 1 e 2

---
+++ day9/part1.js
@@ -0,0 +1,42 @@
+const { report } = require('./input_final')
+
+const nextValues = []
+
+function getLineOfDifferences(line) {
+    const lineOfDifferences = []
+    for (let i = 0; i < line.length - 1; i++) {
+        lineOfDifferences.push(line[i + 1] - line[i])
+    }
+    return lineOfDifferences
+}
+
+function getNextValue(history) {
+    const extrapolate = [history]
+    let lineOfDifferences = history
+    while (true) {
+        const qttZeros = lineOfDifferences.filter((e) => e === 0).length
+        if (qttZeros === lineOfDifferences.length) {
+            break
+        } else {
+            lineOfDifferences = getLineOfDifferences(lineOfDifferences)
+            extrapolate.push(lineOfDifferences)
+        }
+    }
+
+    let lastNumberOfLine = 0
+    for (const line of extrapolate.reverse()) {
+        lastNumberOfLine += line[line.length - 1]
+        line.push(lastNumberOfLine)
+    }
+
+    return lastNumberOfLine
+}
+
+for (const history of report) {
+    nextValues.push(getNextValue(history))
+}
+
+let sumOfNextValues = 0
+for (const value of nextValues) sumOfNextValues += value
+
+console.info('sumOfNextValues:', sumOfNextValues)
